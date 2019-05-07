3c80a52cedfe349da2213b010105a4a463993aed
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-05-07 20:23:45 -0300

    Add ejs e create do folder views e seus files

--- app-express.js
+++ app-express.js
@@ -1,13 +1,17 @@
 var my_express = require('express');
 
 var my_app = my_express();
-// ambas as linhas poderia ser resumidas em: var my_express = 
+/* avisar ao express que ele trabalhará em equipe com EJS
+ * estamos avisando ao xpress que o ejs cuidará da propriedade view engime
+ */
+my_app.set('view engine', 'ejs');
+
 my_app.get('/', (req, resp)=>{
     resp.send('<body><header><title>Express</title></header><body><h1>Home</h1></body></body>')
 });
 
 my_app.get('/tecnologia', (req, resp)=>{
-    resp.send('<body><header><title>Express</title></header><body><h1>Tecnologia</h1></body></body>')
+    resp.render('secao/tecnologia.ejs');
 });
 
 // esse método recebe como argumento uma porta e um callback.
