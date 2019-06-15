7316f83af9e04d3d26328caf11433aac8e2d5710
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-15 11:26:53 -0300

    Acionando conexão com banco de dados e renderizando o conteúdo da página noticias dinâmicamente.

--- app/routes/notices_router.js
+++ app/routes/notices_router.js
@@ -1,6 +1,12 @@
+let dbConection = require('../../config/dbConection');
+
 module.exports = function(my_app) {
+    let my_connection = dbConection();
     // render noticias
-my_app.get('/noticias', (req, resp)=> {
-    resp.render('noticias/noticias.ejs');
-})
+    my_app.get('/noticias', (req, resp)=> {
+        
+        my_connection.query(
+            'select * from noticia', (erro, sucess) => resp.render('noticias/noticias', {noticias: sucess})
+            );
+    })
 }
