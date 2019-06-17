4a20064375296be45419dd6314b38ff17ace3e33
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-17 11:13:18 -0300

    Criação do controller home e de seu módulo index. Configurando uso deste módulo na view home.

---
+++ app/controllers/homeController.js
@@ -0,0 +1,4 @@
+module.exports.index = function(resp) {
+    // o método render é proporcionado pelo ejs.
+    resp.render('home/index.ejs');
+}
--- app/routes/home_router.js
+++ app/routes/home_router.js
@@ -1,6 +1,5 @@
 module.exports = function(my_app) {
     my_app.get('/', (req, resp)=>{
-        // o método render é proporcionado pelo ejs.
-        resp.render('home/index.ejs');
+        my_app.app.controllers.homeController.index(resp);
     });
 }
