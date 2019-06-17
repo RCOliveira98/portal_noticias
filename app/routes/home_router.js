5cd1d0fec908b8259df87e436ae80696d5e87edb
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-17 16:05:40 -0300

    Refatoraçãdas rotas do home.

--- app/routes/home_router.js
+++ app/routes/home_router.js
@@ -1,5 +1,5 @@
 module.exports = function(my_app) {
     my_app.get('/', (req, resp)=>{
-        my_app.app.controllers.homeController.index(resp);
+        my_app.app.controllers.homeController.index(my_app, resp);
     });
 }
