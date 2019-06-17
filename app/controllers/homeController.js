4a51c7a6c3c26884de34475f02c08aebd0384f39
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-17 16:05:01 -0300

    Refatoração do controller home.

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,4 +1,8 @@
-module.exports.index = function(resp) {
+module.exports.index = function(my_app, resp) {
+
+    let my_connection = my_app.config.dbConection();
+    let modelo = new my_app.app.models.NoticiaDao(my_connection);
+
+    modelo.getTopFiveNotices((error, sucess) => resp.render('home/index.ejs', {noticias: sucess}))
     // o método render é proporcionado pelo ejs.
-    resp.render('home/index.ejs');
 }
