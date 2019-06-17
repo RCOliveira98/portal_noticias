3d57c18cf4913285e4d6a471173e85da94eaa328
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-17 11:03:19 -0300

    Adicionando os controlers e configurando seus métodos.

---
+++ app/controllers/adminController.js
@@ -0,0 +1,27 @@
+module.exports.formulario = function(my_app, req, resp){
+    resp.render('admin/form_add_noticia.ejs');
+}
+
+module.exports.save_notice = function(my_app, req, resp) {
+    let msg = 'prenchimento obrigatório';
+        let new_notice = req.body;
+
+        /*
+        req.assert('titulo', msg).notEmpty();
+        req.assert('noticia', msg + ':10 a 100 caracteres').notEmpty().len(10, 100);
+        req.assert('nome_autor', msg).notEmpty();
+        req.assert('resumo', msg).notEmpty();
+        req.assert('data_registro', msg).notEmpty().isDate({format: 'yyyy-mm-dd'});
+
+        if(req.validationsErrors) {
+            resp.render('/noticias/salvar');
+            return;
+        }
+        */
+
+        let my_connection = my_app.config.dbConection();
+
+        let noticiaModel = new my_app.app.models.NoticiaDao(my_connection);
+
+        noticiaModel.saveNotice(new_notice, (erro, sucess) => resp.redirect('/noticias'));
+}
---
+++ app/controllers/noticiaController.js
@@ -0,0 +1,13 @@
+module.exports.get_noticias = function(my_app, req, resp) {
+    let my_connection = my_app.config.dbConection();
+        let noticiaModel = new my_app.app.models.NoticiaDao(my_connection);
+
+        noticiaModel.getNoticiasAll((erro, sucess) => resp.render('noticias/noticias', {noticias: sucess}));
+}
+
+module.exports.get_noticia_by_id = function(my_app, req, resp) {
+    let my_connection = my_app.config.dbConection();
+    let noticiaModel = new my_app.app.models.NoticiaDao(my_connection);
+
+    noticiaModel.getNoticiaById((erro, sucess) => resp.render('noticias/noticia', {noticia: sucess}));
+}
